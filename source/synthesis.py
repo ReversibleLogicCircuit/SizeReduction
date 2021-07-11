268196d0404a98f55611cb3d675b8b9f2ca701f7
ReversibleLogicCircuit <86782509+ReversibleLogicCircuit@users.noreply.github.com>
2021-07-11 23:09:40 +0900

    Add files via upload

---
+++ source/synthesis.py
@@ -0,0 +1,85 @@
+import argparse
+import time
+from core import *
+
+parser = argparse.ArgumentParser(
+    prog="Qube",
+    description="Reversible Logic Circuit Sysnthesis Algorithm",
+    epilog=
+        '''
+        This message is for epliog.
+        ''')
+
+parser.add_argument('path', metavar='File', type=str, help='an target permutation')
+parser.add_argument('nBit', metavar='B', type=int, help='the number of bit')
+parser.add_argument('depths', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+args = parser.parse_args()
+
+# setting for depths
+depths = [0 for i in range(20)]
+for i in range(len(args.depths)):
+    depths[i] = args.depths[i]
+
+# reading a file
+with open(args.path, "r") as fp:
+    data = fp.readline()
+permutation = [int(n) for n in data[1:-1].split(", ")]
+n = args.nBit
+
+# synthesis for reversible logic circuit
+gates = alg_synthesis(n, permutation, depths, True)
+
+# writing the result to .real file
+temp = args.path.split(".")
+temp[-2] += "_out"
+temp = temp[:-1] + ["real"]
+temp = ".".join(temp)
+writeRealFormat(n, gates, temp)
+
+#####################################################
+
+# printing related informations
+print("Target file\t:", args.path)
+print("# of bits\t:", args.nBit)
+print("Exha. depths\t:", depths[:args.nBit-1])
+
+# counting the number of Toffoli gate and adjuting to permutation
+cost = 0
+for i in range(0, len(gates[:-1]), 3):
+    # non- interrupting 
+    for gate in gates[i]:
+        if len(gate[1]) > 1:
+            cost += 2 * len(gate[1]) - 3
+        permutation = apply_gate(n, permutation, gate)
+    
+    # decompising
+    for j in range(0, len(gates[i+1]), 2):
+        tempCost = 0
+        for gate in gates[i+1][j]:
+            if len(gate[1]) > 1:
+                tempCost += 2 * len(gate[1]) - 3
+            permutation = apply_gate(n, permutation, gate)
+        for gate in gates[i+1][j+1]:
+            if len(gate[1]) > 1:
+                tempCost += 2 * len(gate[1]) - 3
+            permutation = apply_gate(n, permutation, gate)
+        cost += tempCost
+    
+    # parity
+    for gate in gates[i+2]:
+        if len(gate[1]) > 1:
+            cost += 2 * len(gate[1]) - 3
+        permutation = apply_gate(n, permutation, gate)
+
+# 2-bits
+for gate in gates[-1]:
+    if len(gate[1]) > 1:
+        cost += 2 * len(gate[1]) - 3
+    permutation = apply_gate(n, permutation, gate)
+
+# checking resulting permutation is equal to identity
+if permutation != list(range(1 << n)):
+    print("result is not equal!!!")
+    print(permutation)
+
+print("Number of Toffoli gate:", cost)
